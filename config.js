165539a8a3fb8de4878450cefc58d8009dcba8a6
LORD PREMO <cypherx599@gmail.com>
2026-02-28 18:09:19 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+// config.js — BROKEN LORD MD
+
+const config = {
+    bot: {
+        name: "BROKEN LORD MD",
+        version: "1.0.0"
+    },
+    mode: "private",
+    projectOwner: {
+        name: "BROKENLORD",
+        number: "255773001107"
+    },
+    prefix: [".", "!", "/"],
+    defaultPrefix: ".",
+    defaults: {
+        antilink: false,
+        antibadword: false,
+        antidelete: false,
+        welcome: false,
+        badwords: []
+    }
+};
+
+export default config;
